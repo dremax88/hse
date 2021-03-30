ddf7911eee94f4cf0b20b9872362f064d34e5284
Dremax <ramazan.batyaev@yandex.ru>
2021-03-30 12:43:45 +0300

    1 step

---
+++ vendor/composer/installed.php
@@ -0,0 +1,26 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'dev-requirement' => true,
+    'name' => 'vendor_name/hse',
+  ),
+  'versions' => 
+  array (
+    'vendor_name/hse' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+      'dev-requirement' => false,
+    ),
+  ),
+);
